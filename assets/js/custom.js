0210ad08e5b8288f84e7b89fc8b257a44dcc9c91
Maxwell Morrison <maxkobbs@gmail.com>
2019-02-14 17:28:43 +0000

    Finalized development

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -17,7 +17,7 @@
   })
 
 // Set the date we're counting down to
-var countDownDate = new Date("Sept 28, 2018 00:00:00").getTime();
+var countDownDate = new Date("Sept 28, 2019 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
